c7756e301a9be22f472b4db45e53731f013450b4
Uladzimir1995 <bronov1995@gmail.com>
2019-04-15 21:33:28 +0300

    Task1 Commit
    Task 1

---
+++ HOME/src/Task1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args){
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Input the String");
+
+        String str=scan.nextLine();
+
+        System.out.println("The String is:"+str);
+
+    }
+}
